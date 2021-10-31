977a01f3d9bb3bf8ba395e240e9f2188b00aec3f
Quinn Paddock <91283358+QuinnPaddock@users.noreply.github.com>
2021-10-30 19:55:59 -0700

    Add files via upload

---
+++ SCIPP_CSV_Parse.py
@@ -0,0 +1,26 @@
+import csv
+
+#need to extract the fields "EPC" and "Local Name" from the csv file "module frames 23june2021.csv"
+filename = 'module frames 23june2021.csv'
+
+with open(filename, 'r', newline='') as csv_file:
+	reader = csv.reader(csv_file)
+	fields = next(reader)	
+	rows = list(reader)		
+
+
+
+for column_number, field in enumerate(fields): 
+	if(field == 'EPC'):
+		print(field)
+		for row in rows:
+			print(row[column_number])
+
+		print('\n')
+
+	elif(field == 'Local Name'):
+		print(field)
+		for row in rows:
+			print(row[column_number])
+
+		print('\n')
