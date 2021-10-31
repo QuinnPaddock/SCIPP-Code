faa897e55b4edd676aff1103636071766f0b1140
Quinn Paddock <91283358+QuinnPaddock@users.noreply.github.com>
2021-10-30 20:23:15 -0700

    Add files via upload

--- SCIPP_CSV_Parse.py
+++ SCIPP_CSV_Parse.py
@@ -2,9 +2,9 @@ import csv
 
 #need to extract the fields "EPC" and "Local Name" from the csv file "module frames 23june2021.csv"
 
-filename = 'module frames 23june2021.csv' #this will need to be changed based on what
-										  #the file is called on whatever computer
+filename = 'module frames 23june2021.csv' #this will need to be changed based on what the file is called on whatever computer
 
+										  
 with open(filename, 'r', newline='') as csv_file:
 	reader = csv.reader(csv_file)
 	fields = next(reader)	
